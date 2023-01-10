171272e5a52eb10107709214cafd82b976aa25bd
flyether <flyether@gmail.com>
2023-01-10 13:37:40 +0300

    fix: jest config change to js

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+
+const jestConfig = {
+  
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
+
+export default jestConfig
